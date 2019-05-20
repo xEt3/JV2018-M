2c7eba89df257803749b248296bfae7e77d7c58b
ajp <ajp@NOX.cierva.local>
2019-05-20 09:16:53 +0200

    Clase principal con id de usuario en la linea de comandos.

--- JV2018-M/src/java/JVPrincipal.java
+++ JV2018-M/src/java/JVPrincipal.java
@@ -1,9 +1,10 @@
 /** 
  * Proyecto: Juego de la vida.
- * Implementación del control de inicio de sesión y ejecución de la simulación por defecto. 
+ * Implementación del control de inicio de sesión y ejecución de la simulación por defecto.
+ * Permite incluir id de usuario en la línea de comandos.
  * @since: prototipo1.0
  * @source: JVPrincipal.java 
- * @version: 2.1 - 2019/05/06
+ * @version: 2.1 - 2019/05/16
  * @author: ajp
  */
 
@@ -12,6 +13,10 @@ import accesoUsr.consola.control.ControlPrincipal;
 public class JVPrincipal {
 
 	public static void main(String[] args) {		
+		if (args.length > 0) {
+			new ControlPrincipal(args[0]);
+			return;
+		}
 		new ControlPrincipal();
 	}
 	
