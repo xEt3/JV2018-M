9778ead0ca1cef3e33a85273698ebcd272bc8f81
xEt3 <xet3@outlook.es>
2019-09-01 21:54:21 +0200

    terminada la clase DAO sql de sesiones, falta probar, mañana mas:)

--- JV2018-M/src/java/accesoDatos/mySql/SesionesDAO.java
+++ JV2018-M/src/java/accesoDatos/mySql/SesionesDAO.java
@@ -17,9 +17,12 @@ package accesoDatos.mySql;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.table.DefaultTableModel;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
@@ -29,6 +32,7 @@ import accesoDatos.DatosException;
 import accesoDatos.OperacionesDAO;
 import modelo.*;
 import modelo.SesionUsuario.EstadoSesion;
+import modelo.Simulacion.EstadoSimulacion;
 import util.*;
 
 public class SesionesDAO implements OperacionesDAO {
@@ -39,18 +43,15 @@ public class SesionesDAO implements OperacionesDAO {
 	// Base de datos
 	private Connection db;
 
-	// Fachada de datos
-	private Datos datos;
-
 	// Atributos de procesamiento de la base de datos.
 	private Statement stSesiones;
 	private ResultSet rsSesiones;
+	private DefaultTableModel tmSesiones;
+	private ArrayList<SesionUsuario> bufferSesiones;
 
 	// Constructor
 	private SesionesDAO() {
 		db = Conexion.getDB();
-		datos = new Datos();
-
 		try {
 			inicializar();
 		} catch (SQLException e) {
@@ -92,11 +93,12 @@ public class SesionesDAO implements OperacionesDAO {
 	 */
 	private void crearTablaSesiones() {
 		try {
-			stSesiones.executeUpdate("CREATE TABLE IF NOT EXISTS `sesiones` ("
-								+ "`id_usuario` VARCHAR(45) NOT NULL,"
-								+ "`fecha` DATETIME NOT NULL,"
-								+ "`estado` VARCHAR(20) NOT NULL,"
-								+ "PRIMARY KEY (`id_usuario`, `fecha`))");
+			stSesiones.executeUpdate("CREATE TABLE IF NOT EXISTS SESIONES("
+								+ "id_sesion VARCHAR(255) NOT NULL PRIMARY KEY,"
+								+ "id_usuario VARCHAR(45) NOT NULL,"
+								+ "fecha DATETIME NOT NULL,"
+								+ "estado VARCHAR(20) NOT NULL,"
+								+ ")");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -109,94 +111,150 @@ public class SesionesDAO implements OperacionesDAO {
 	 * @return SesionUsuario si la encuentra
 	 */
 	@Override
-	public SesionUsuario obtener(String idSesion) {
-		List<SesionUsuario> lista = new ArrayList<SesionUsuario>();
-		try {
-			rsSesiones = stSesiones.executeQuery("SELECT * FROM sesiones "
-					+ "WHERE CONCAT(id_usuario,':',DATE_FORMAT(fecha,'%Y%m%d%H%i%s'))=" + idSesion);
-			lista = obtener(rsSesiones);
-			if (lista.size() > 0) {
-				return lista.get(0);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+	public SesionUsuario obtener(String id) {
+		assert id != null;
+		ejecutarConsulta(id);
+		etiquetarColumnasModelo();
+		borrarFilasModelo();
+		rellenarFilasModelo();
+		sincronizarBufferSimulaciones();
+
+		if (bufferSesiones.size() > 0) {
+			return (SesionUsuario) bufferSesiones.get(0);
 		}
 		return null;
 	}
 
+	private void ejecutarConsulta(String id) {
+		try {
+			rsSesiones = stSesiones.executeQuery("select * from SESIONES where id_sesion = '" + id + "'");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void etiquetarColumnasModelo() {
+		try {
+			ResultSetMetaData metaDatos = this.rsSesiones.getMetaData();
+			// numero total de columnas
+			int numCol = metaDatos.getColumnCount();
+			// etiqueta de cada columna
+			Object[] etiquetas = new Object[numCol];
+			for (int i = 0; i < numCol; i++) {
+				etiquetas[i] = metaDatos.getColumnLabel(i + 1);
+			}
+			// Incorpora array de etiquetas en el TableModel.
+			this.tmSesiones.setColumnIdentifiers(etiquetas);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void borrarFilasModelo() {
+		while (this.tmSesiones.getRowCount() > 0) {
+			this.tmSesiones.removeRow(0);
+		}
+	}
+	
+	private void rellenarFilasModelo() {
+		Object[] datosFila = new Object[this.tmSesiones.getColumnCount()];
+		try {
+			while(rsSesiones.next()) {
+				for (int i = 0; i < this.tmSesiones.getColumnCount(); i++) {
+					datosFila[i] = this.rsSesiones.getObject(i+1);
+				}
+				this.tmSesiones.addRow(datosFila);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void sincronizarBufferSimulaciones() {
+		bufferSesiones.clear();
+		for (int i = 0; i < tmSesiones.getRowCount(); i++) {
+			try {
+				String id_sesion = (String) tmSesiones.getValueAt(i, 0);
+				String id_usuario = (String) tmSesiones.getValueAt(i, 1);
+				Fecha fecha  = obtenerFechaFromDateTime(tmSesiones.getValueAt(i, 2).toString());
+				EstadoSesion estado = obtenerEstadoSesion((String)tmSesiones.getValueAt(i, 5));
+				UsuariosDAO usrDAO = UsuariosDAO.getInstance();
+				bufferSesiones.add(new SesionUsuario(id_sesion,usrDAO.obtener(id_usuario),fecha,estado));
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Metodo que combierte el resultado que se obtiene de una consulta de un datetime a un objeto Fecha
+	 * @param dateTime
+	 * @return
+	 */
+	private Fecha obtenerFechaFromDateTime(String dateTime) {
+		String[] fechaYHora= dateTime.split(" ");
+		String[] fecha = fechaYHora[0].split("-");
+		String[] hora = fechaYHora[1].split(":");
+		return new Fecha(Integer.parseInt(fecha[0]),Integer.parseInt(fecha[1]),Integer.parseInt(fecha[2]),
+				Integer.parseInt(hora[0]),Integer.parseInt(hora[1]),(int)Double.parseDouble(hora[2]));
+	}
+	
+	/**
+	 * Metodo para convertir un varchar en un enum
+	 * @param valorSinFormatear
+	 * @return
+	 */
+	private EstadoSesion obtenerEstadoSesion(String valorSinFormatear) {
+		if(valorSinFormatear.equals("ACTIVA")) {
+			return EstadoSesion.ACTIVA;
+		}
+		if(valorSinFormatear.equals("CERRADA")) {
+			return EstadoSesion.CERRADA;
+		}
+		if(valorSinFormatear.equals("EN_PREPARACION")) {
+			return EstadoSesion.EN_PREPARACION;
+		}
+		return null;
+	}
+	
 	/**
 	 * Obtención de la lista de Sesiones almacenadas.
 	 * 
 	 * @return lista de Sesiones de Usuario
 	 */
 	@Override
-	public List<SesionUsuario> obtenerTodos() {
-		List<SesionUsuario> lista = new ArrayList<SesionUsuario>();
+	public ArrayList<SesionUsuario> obtenerTodos() {
 		try {
-			rsSesiones = stSesiones.executeQuery("SELECT * FROM sesiones");
-			lista = obtener(rsSesiones);
+			rsSesiones = stSesiones.executeQuery("select * from SESIONES");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return lista;
+		etiquetarColumnasModelo();
+		borrarFilasModelo();
+		rellenarFilasModelo();
+		sincronizarBufferSimulaciones();;
+		return this.bufferSesiones;
 	}
-
+	
 	/**
 	 * Obtiene todas las sesiones por IdUsr de usuario.
 	 * 
 	 * @param idUsr - el idUsr a buscar.
 	 * @return - las sesiones encontradas.
 	 */
-	public List<SesionUsuario> obtenerTodasMismoUsr(String idUsr) {
-		List<SesionUsuario> lista = new ArrayList<SesionUsuario>();
+	public ArrayList<SesionUsuario> obtenerTodasMismoUsr(String idUsr) {
 		try {
-			rsSesiones = stSesiones.executeQuery("SELECT * FROM sesiones WHERE id_usuario=" + idUsr);
-			lista = obtener(rsSesiones);
+			rsSesiones = stSesiones.executeQuery("select * from SESIONES  where id_usuario = '" + idUsr + "'");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return lista;
+		etiquetarColumnasModelo();
+		borrarFilasModelo();
+		rellenarFilasModelo();
+		sincronizarBufferSimulaciones();
+		return this.bufferSesiones;
 	}
-
-	/**
-	 * Método obtener que se encarga de buscar en la base de datos según el tipo de
-	 * identificador que le es introducido. Creado para evitar repetición de código.
-	 * 
-	 * @param id - identificador a buscar, si contiene ":" lo detectará como id de
-	 *           sesión, si no, como id de usuario, y si es nulo, devolverá todas
-	 *           las sesiones que existan.
-	 * @return Lista de Sesiones de usuario encontradas
-	 */
-	private List<SesionUsuario> obtener(ResultSet resultadoQuery) {
-		ArrayList<SesionUsuario> listaSesiones = new ArrayList<SesionUsuario>();
-		Usuario usr = null;
-
-		try {
-			while (resultadoQuery.next()) {
-				usr = datos.obtenerUsuario((resultadoQuery.getString("id_usuario")));
-				String estadoString = resultadoQuery.getString("estado");
-				EstadoSesion estado = null;
-				switch (estadoString) {
-				case "EN_PREPARACION":
-					estado = SesionUsuario.EstadoSesion.EN_PREPARACION;
-					break;
-				case "ACTIVA":
-					estado = SesionUsuario.EstadoSesion.ACTIVA;
-					break;
-				case "CERRADA":
-					estado = SesionUsuario.EstadoSesion.CERRADA;
-					break;
-				}
-
-				listaSesiones.add(new SesionUsuario(usr, new Fecha(resultadoQuery.getString("fecha")), estado));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return listaSesiones;
-	}
-
+	
 	/**
 	 * Alta de una nueva SesionUsuario.
 	 * 
@@ -206,36 +264,50 @@ public class SesionesDAO implements OperacionesDAO {
 	@Override
 	public void alta(Object obj) throws DatosException {
 		assert obj != null;
-		SesionUsuario sesionNueva = (SesionUsuario) obj;
-
-		if (obtener(sesionNueva.getId()) == null) {
-
-			// insert sesion
-			String query = "INSERT INTO sesiones VALUES (?,?,?);";
-			Timestamp ts = new Timestamp(sesionNueva.getFecha().getMarcaTiempoMilisegundos());
-
+		SesionUsuario sesion= (SesionUsuario)obj;
+		String sqlQuery = obtenerConsultaAlta(sesion);
+		if (obtener(sesion.getId()) == null) {
 			try {
-				// preparar inserción
-				PreparedStatement prepStm = db.prepareStatement(query);
-				prepStm.setString(1, sesionNueva.getUsr().getId());
-				prepStm.setTimestamp(2, ts);
-				prepStm.setString(3, sesionNueva.getEstado().toString());
-
-				// insertar
-				prepStm.execute();
-
-				db.close();
-
-			} catch (SQLException e) {
+				Statement statement = db.createStatement();
+				statement.executeUpdate(sqlQuery);
+			} 
+			catch (SQLException e) {
 				e.printStackTrace();
 			}
-
-			//
-		} else {
-			throw new DatosException("SesionesDAO.alta: " + sesionNueva.getId() + " ya existe");
+		}
+		else {
+			throw new DatosException("Sesion dada de Alta ya existe con el nombre: " + sesion.getId());
 		}
 	}
-
+	
+	/**
+	 * Consulta que se lanza para dar de alta el mundo en la base de datos.
+	 * @param mundo
+	 * @return - Devuelve la QuerySQL para dar de alta el mundo.
+	 */
+	private String obtenerConsultaAlta(SesionUsuario sesion) {
+		StringBuilder sqlQuery = new StringBuilder();
+		sqlQuery.append("INSERT INTO SESIONES (id_sesion,id_usuario,fecha,estado) VALUES"
+				+ "('" + sesion.getId()+ "'," 
+				+"'"+ sesion.getUsr().getId()+ "'," 
+				+"'"+ sesion.getFecha()+ "',"  
+				+ "'"+ formatearEstadoSesion(sesion.getEstado()) +"');");
+		return sqlQuery.toString();
+	}
+	
+	private String formatearEstadoSesion(EstadoSesion estadoSesion) {
+		if(estadoSesion.equals(EstadoSesion.ACTIVA)) {
+			return "ACTIVA";
+		}
+		if(estadoSesion.equals(EstadoSesion.CERRADA)) {
+			return "CERRADA";
+		}
+		if(estadoSesion.equals(EstadoSesion.EN_PREPARACION)) {
+			return "EN_PREPARACION";
+		}
+		return null;
+	}
+	
 	/**
 	 * Baja de una sesion existente.
 	 * 
@@ -246,30 +318,22 @@ public class SesionesDAO implements OperacionesDAO {
 	@Override
 	public SesionUsuario baja(String id) throws DatosException {
 		assert id != null;
-		SesionUsuario sesionAborrar = (SesionUsuario) obtener(id);
-
-		if (sesionAborrar == null) {
-			throw new DatosException("SesionesDAO.baja: id " + id + "no encontrada");
-		} else {
-			String query = "DELETE FROM sesiones where CONCAT(id_usuario,':',DATE_FORMAT(fecha,'%Y%m%d%H%i%s')) = ?";
-
+		assert !id.matches("");
+		assert !id.matches("[ ]+");
+		SesionUsuario sesion = (SesionUsuario) obtener(id);
+		if (sesion != null) {
 			try {
-				// preparar borrado
-				PreparedStatement prepStm = db.prepareStatement(query);
-				prepStm.setString(1, sesionAborrar.getId());
-
-				// borrar tupla
-				prepStm.execute();
-
-				db.close();
-
+				bufferSesiones.remove(sesion);
+				String sql = "DELETE FROM SESIONES WHERE id_sesion ='" + id + "'";
+				stSesiones.executeUpdate(sql);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+			return sesion;
 		}
-		return sesionAborrar;
+		throw new DatosException("Baja: " + id + " no existe...");
 	}
-
+	
 	/**
 	 * Actualiza los datos de una sesion.
 	 * 
@@ -279,29 +343,36 @@ public class SesionesDAO implements OperacionesDAO {
 	@Override
 	public void actualizar(Object obj) throws DatosException {
 		assert obj != null;
-		SesionUsuario sesNuevoEstado = (SesionUsuario) obj;
-
-		if (obtener(sesNuevoEstado.getId()) == null) {
-			throw new DatosException("SesionesDAO.baja: id " + sesNuevoEstado.getId() + "no encontrada");
-		} else {
-			String query = "UPDATE sesiones SET estado = ? WHERE CONCAT(id_usuario,':',DATE_FORMAT(fecha,'%Y%m%d%H%i%s')) = ?";
+		SesionUsuario sesionActualizada = (SesionUsuario) obj;
+		SesionUsuario sesionPrevia = (SesionUsuario) obtener(sesionActualizada.getId());
+		String sqlQuery = obtenerConsultaActualizar(sesionActualizada);
+		if (sesionPrevia != null) {
 			try {
-				// preparar actualización
-				PreparedStatement prepStm = db.prepareStatement(query);
-				prepStm.setString(1, sesNuevoEstado.getEstado().name());
-				prepStm.setString(2, sesNuevoEstado.getId());
-
-				// actualizar tupla
-				prepStm.execute();
-
-				db.close();
-
-			} catch (SQLException e) {
+				this.bufferSesiones.remove(sesionPrevia);
+				this.bufferSesiones.add(sesionActualizada);
+				this.stSesiones.executeUpdate(sqlQuery);
+			}
+			catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+		throw new DatosException("Actualizar: "+ sesionActualizada.getId() + " no existe.");
 	}
-
+	/**
+	 * Consulta que se lanza para actualizar la sesion de la base de datos.
+	 * @param sesionActualizada
+	 * @return - Devuelve la QuerySQL para actualizar la sesion.
+	 */
+	private String obtenerConsultaActualizar(SesionUsuario sesionActualizada) {
+		String id_usr = sesionActualizada.getUsr().getId();
+		Fecha fecha= sesionActualizada.getFecha();
+		EstadoSesion estado = sesionActualizada.getEstado();
+		StringBuilder query = new StringBuilder();
+		query.append("UPDATE SESIONES SET id_usuario = '" + id_usr + "', fecha  = '" + fecha.getGregorian() +  "', estado = '" + formatearEstadoSesion(estado)
+				+ "' WHERE id_sesion LIKE'"+ sesionActualizada.getId()+"'");
+		return query.toString();
+	}
+	
 	/**
 	 * Devuelve los datos de todas las Sesiones de usuario como String
 	 * 
@@ -314,10 +385,9 @@ public class SesionesDAO implements OperacionesDAO {
 		for (SesionUsuario sesionUsuario : listaSsn) {
 			result.append(sesionUsuario.toString() + ("\n"));
 		}
-
 		return result.toString();
 	}
-
+	
 	/**
 	 * Devuelve las IDs de todas las Sesiones de usuario como String
 	 * 
@@ -327,26 +397,24 @@ public class SesionesDAO implements OperacionesDAO {
 	public String listarId() {
 		StringBuffer result = new StringBuffer();
 		List<SesionUsuario> listaSsn = obtenerTodos();
-		for (SesionUsuario sesionUsuario : listaSsn) {
-			result.append(sesionUsuario.getId() + ("\n"));
+		for (SesionUsuario sesion: listaSsn) {
+			result.append(sesion.getId() + ("\n"));
 		}
 
 		return result.toString();
 	}
-
+	
 	/**
 	 * Elimina todas los objetos SesionUsuario en la base de datos.
 	 */
 	@Override
 	public void borrarTodo() {
-		List<SesionUsuario> listaSesiones = obtenerTodos();
-		for (SesionUsuario sesion : listaSesiones) {
-			try {
-				baja(sesion.getId());
-			} catch (DatosException e) {
-				e.printStackTrace();
-			}
+		try {
+			stSesiones.executeUpdate("DELETE FROM SESIONES");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-
+	
 }
